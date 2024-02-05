5a5bdeb34614e9577aef9b13d65c91d6cfaba9eb
Alexander Braun <abraun@rml-legion.mpi.localnet>
2024-02-05 17:59:57 +0100

    New architecture with NVIDIA streaming STT, Phi-2 and StyleTTS2

--- main.py
+++ main.py
@@ -1,225 +1,175 @@
-import speech_recognition as sr        # Importing the speech recognition library
-from llama_cpp import Llama           # Importing the Llama library for large language model processing
-from TTS.api import TTS               # Importing the Text-to-Speech API
-import whisper                         # Importing the Whisper library for speech recognition
-import sounddevice as sd              # Importing the library for sound device manipulation
-import time                            # Importing the time library for handling time-related tasks
-import threading                       # Importing the threading library for concurrent execution
-import queue                           # Importing the queue library for queue data structure
-import pyaudio                         # Importing the PyAudio library for audio I/O
-import argparse                        # Importing the argparse library for command-line argument parsing
-
-# Constants for configuration
-WHISPER_MODEL = "base"
-LANGUAGE = "en"
-LLM_PATH = "dolphin-2_6-phi-2.Q4_K_M.gguf" #"stablelm-zephyr-3b.Q4_K_M.gguf"
-SPEAKER_WAV = "voice_male.wav"
-TTS_MODEL = "tts_models/multilingual/multi-dataset/xtts_v2"
-DEVICE = "cuda"
-STOP_PHRASE = "Stop recording"
-
-PRINT_TIME_MEASUREMENTS = False
+import gc
+import time
+import torch
+import pyaudio
+import multiprocessing
+import sounddevice as sd
+from preprocessor import Preprocessor
+from streaming_buffer import StreamBuffer
+from stt_llm_tts_model import STT_LLM_TTS
 
 
-class Voice2Voice():
-    # Audio format settings for PyAudio
-    FORMAT = pyaudio.paFloat32
-    CHANNELS = 1
-    RATE = 24000
+def record(audio_buffer, start_recording):
+    """Record an audio stream from the microphone in a separate process  
+        Args:
+            audio_buffer: multiprocessing queue to store the recorded audio data
+            start_recording: multiprocessing value to start and stop the recording
+    """
+    RATE = 16000
+    CHUNK = 2048
 
-    def __init__(self, whisper_model, llm, tts,voice_file,  wake_word=None):
-        # Initialization of the Voice2Voice class
-        self.recognizer = sr.Recognizer()  # Speech recognition instance
-        self.recognizer.pause_threshold = 0.5  # Configuration for pause threshold
-        self.whisper_model = whisper_model  # Whisper model for speech recognition
-        self.llm = llm  # Large language model (LLM) instance
-        self.tts = tts  # Text-to-Speech (TTS) instance
-        self.tts_model = self.tts.synthesizer.tts_model  # TTS model
-        # Getting conditioning latents for TTS
-        self.gpt_cond_latent, self.speaker_embedding = self.tts_model.get_conditioning_latents(audio_path=[voice_file])
-        self.audio = pyaudio.PyAudio()  # PyAudio instance for audio I/O
-        self.audio_buffer = queue.Queue()  # Queue for audio buffer
-        self.text_buffer = queue.Queue()  # Queue for text buffer
-        self.prompt_buffer = queue.Queue() # Queue for user transcribed user input
-        self.wake_word = wake_word  # Optional wake word for activation
-        self.start_conversation = time.time()
+    # Open audio input stream
+    audio = pyaudio.PyAudio()
+    streamIn = audio.open(format=pyaudio.paFloat32, channels=1,
+                            rate=RATE, input=True, input_device_index=0,
+                            frames_per_buffer=CHUNK)
+    
+    while(True):
+        try:
+            # start_recording is set to 1 in the main loop to start the recording
+            if start_recording == 0:
+                time.sleep(0.1)
+                continue
+            # read a chunk of fixed size from the input stream and add it to the input buffer 
+            data = streamIn.read(CHUNK, exception_on_overflow=False)
+            audio_buffer.put(data)
 
-        self.stop_llm_inference = False
-        self.stop_tts_inference = False
-        self.context = "System: You are a helpfull assistent that talks to a user. Keep your answers short and simple."
-
-    def start_threads(self):
-        # Start threads for text processing and audio playback
-        tts_thread = threading.Thread(target=self.tts_thread)
-        tts_thread.setDaemon(True)
-        tts_thread.start()
-
-        audio_thread = threading.Thread(target=self.play_audio_thread)
-        audio_thread.setDaemon(True)
-        audio_thread.start()
+        except KeyboardInterrupt:
+            return
         
-        llm_thread = threading.Thread(target=self.llm_thread)
-        llm_thread.setDaemon(True)
-        llm_thread.start()
+        except Exception as e:
+            raise e 
+        
+def play_audio(audio_output_buffer):
+    """Play synthesized audio data in a separate process  
+        Args:
+            audio_output_buffer: multiprocessing-queue to receive audio data
+    """
+    fs = 24000
+    while(True):
+        # get next audio data 
+        wav = audio_output_buffer.get()
+        # play the audio and wait until it is finished (only this sub process is blocked, not the main loop)
+        sd.play(wav, fs, blocking=True) 
 
-    def transcribe_audio(self,whisper_model):
-        # Transcribe audio using the Whisper model
-        print("\nASR: Transcribing ...")
-        stop = False
-        start = time.time()
-        transcription = whisper_model.transcribe("audio.wav", language=LANGUAGE, fp16=False)
-        if len(transcription["segments"])>0 and transcription["segments"][0]['no_speech_prob'] < 0.2:
-            text = transcription["text"]
-            if STOP_PHRASE in text.lower():
-                stop = True
-        else:
-            text = None
-        if PRINT_TIME_MEASUREMENTS:
-            print("ASR: <T> ", time.time()-start)
-        print("ASR --> ", text)
-        return text, stop
+def flush():
+  """Flush Cuda cache to prevent side effect and slowdowns   
+  """
+  gc.collect()
+  torch.cuda.empty_cache()
+  torch.cuda.reset_peak_memory_stats()
 
-    def synthesize_speech(self, text):
-        print("\nTTS: synthesizing... ")
-        start = time.time()
-        stream_generator = self.tts_model.inference_stream(
-            text,
-            LANGUAGE,
-            self.gpt_cond_latent,
-            self.speaker_embedding)
-        for i, chunk in enumerate(stream_generator):
-            if self.stop_tts_inference:
-                self.stop_tts_inference = False
+def main_loop(streaming_buffer, model, audio_input_buffer, audio_output_buffer,  start_recording):
+    """Wait for audio input, call voice assistant model and play synthesized speech  
+        Args:
+            streaming_buffer: streaming buffer instance to store preprocessed audio chunks
+            model: instance of STT_LLM_TTS model
+            audio_input_buffer: multiprocessing queue for audio input
+            audio_output_buffer: multiprocessing queue for audio output
+            start_recording: multiprocessing value to start recording of audio chunks
+    """
+    # init preprocessor and streaming iterator
+    preprocessor = Preprocessor()
+    streaming_buffer_iter = iter(streaming_buffer)
+    
+    # send signal to recording process to start the recording
+    start_recording.value = 1
+
+    # control buffer stream id for first chunk 
+    first = True
+
+    # start main loop
+    while True:
+
+        # get as many audio chunks from the buffer as possible. If the buffer is empty, an exception is thrown 
+        # and the inner loop breaks
+        while True:
+            # select stream id (-1) for first chunk (0) else
+            if first:
+                stream_id = -1
+                first = False
+            else:
+                stream_id = 0
+
+            # try to get the next audio chunk, if buffer is empty an exception is thrown 
+            try:
+                # get audio data from buffer
+                data = audio_input_buffer.get(block=False)
+                
+                # preprocess audio data
+                t = torch.frombuffer(data, dtype=torch.float32)
+                t = torch.unsqueeze(t,0)
+                length = torch.tensor([t.shape[1]], dtype=torch.float32)
+                processed_signal, _ = preprocessor(t, length)
+
+                # add processed audio chunks to the streaming buffer 
+                streaming_buffer.append_processed_signal(processed_signal, stream_id=stream_id)
+            except Exception as e:
+                # leave inner loop and process received data
                 break
-            if PRINT_TIME_MEASUREMENTS:
-                print("TTS: <T>", time.time()-start)
-                print("TTS: --> Audio Chunk [",i,"] added to buffer")
-                if i == 0:
-                    print("\n\n\n###############")
-                    print("  Total Latency for first response: ")
-                    print("  [ ", time.time()-self.start_conversation," seconds ]")
-                    print("###############\n\n\n")
-            start = time.time()
-            chunk = chunk.numpy().tobytes()
-            self.audio_buffer.put(chunk)
+                
+        # check if enough audio chunks were recorded for a forward path
+        if streaming_buffer.buffer is not None and streaming_buffer.buffer.size(-1) > streaming_buffer.buffer_idx + streaming_buffer.shift_size:
+            # --> enough chunks are available 
 
-    def play_audio_thread(self):
-        # Thread for playing audio
-        stream = self.audio.open(format=self.FORMAT, channels=self.CHANNELS,
-                            rate=self.RATE, output=True)
-        while True:
-            data = self.audio_buffer.get()
-            stream.write(data)
-
-    def tts_thread(self):
-        # Thread for processing text-to-speech
-        while True:
-            text = self.text_buffer.get()
-            if not text is None:
-                self.synthesize_speech(text)
+            # get preprocessed audio chunks from buffer
+            data = next(streaming_buffer_iter, None)
+            if data is None: 
+                break
+            chunk_audio, chunk_lengths = data
             
-    def llm_thread(self):
-        while True:
-            prompt = self.prompt_buffer.get()
-            if not prompt is None:
-                self.prompt_llm(prompt)
+            # call model and pass preprocessed audio data
+            chunk_audio = chunk_audio.to("cuda")
+            chunk_lengths = chunk_lengths.to("cuda")
+            text, wav, interrupt = model(chunk_audio, chunk_lengths) 
+        else:
+            # --> not enough chunks. Call model with empty input to generate text
+            text, wav, interrupt = model(None, None)
 
-    def prompt_llm(self,prompt):
-        # Process text with the large language model
+        # TODO: Implement interrup behavior to stop audio process when user starts speaking
 
-        self.context += "\nUser: "+prompt+"\nAssistant:"
+        # model return is None except when a new sentence is generated and synthesized 
+        if text is not None:
+            # --> A new sentence is finished
+            print(text.replace("\n", ""))
 
-        print("\n\n----------------------------")
-        print("CONTEXT: ")
-        print(self.context)
-        print("----------------------------\n\n")
-
-
-        print("\nLLM: inference ...")
-        start = time.time()
-        stream = self.llm(
-            self.context,
-            max_tokens=10000,  
-            stream=True,
-        )
-        output_buffer = ""
-        for output in stream:
-            if self.stop_llm_inference:
-                self.stop_llm_inference = False
-                break
-
-            if "choices" in output and output["choices"]:
-                text_output = output["choices"][0]["text"]
-                output_buffer += text_output
-
-                if any([t in output_buffer for t in [".","!","?"]]):
-                    if len(output_buffer) < 3:
-                        output_buffer = ""
-                        continue
-                    output_buffer = output_buffer.replace('assistant', '').strip() 
-                    if PRINT_TIME_MEASUREMENTS:
-                        print("LLM: <T> ", time.time()-start)
-                    print("LLM: -->  ", output_buffer)
-                    start = time.time()
-                    self.text_buffer.put(output_buffer)
-                    self.context += " "+output_buffer
-                    output_buffer = ""
-
-    def handle_voice_input(self):
-        # Handle voice input from the user
-        self.start_conversation = time.time()
-        text, stop = self.transcribe_audio(self.whisper_model)
-        if self.wake_word != None and self.wake_word.lower() not in text.lower():
-            return False
-        if text != None and not stop:
-            self.interrupt_inference()
-            self.prompt_buffer.put(text)
-        return stop
-
-    def interrupt_inference(self):
-        if self.prompt_buffer.qsize()>0 or self.text_buffer.qsize()>0 or self.audio_buffer.qsize()>0:
-            print("\n## INTERRUPT CURRENT INFERENCE ##")
-            self.stop_llm_inference = True
-            self.stop_tts_inference = True
-            self.prompt_buffer.queue.clear()
-            self.text_buffer.queue.clear()
-            self.audio_buffer.queue.clear()
-
-    def run(self):
-        # Main run loop for the voice assistant
-        self.start_threads()
-
-        self.text_buffer.put("How can I help you?")
-
-        with sr.Microphone() as source:
-            self.recognizer.adjust_for_ambient_noise(source, duration=0.4)
-            print("\n\nListening ...")
-            while True:
-                audio = self.recognizer.listen(source)
-                with open("audio.wav", "wb") as f:
-                    f.write(audio.get_wav_data())
-                    stop = self.handle_voice_input()
-                    if stop:
-                        break
-
+            # Put synthesized audio to output buffer which will be played by the play-audio process
+            audio_output_buffer.put(wav)
+        
+        time.sleep(0.001) # TODO Is this really needed?
+                
 def main():
-    # Main function to set up and run the voice assistant
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--llm", type=str, default=LLM_PATH)
-    parser.add_argument("-w", "--whisper", type=str, default=WHISPER_MODEL)
-    parser.add_argument("-t", "--tts", type=str, default=TTS_MODEL)
-    parser.add_argument("-d", "--device", type=str, default=DEVICE)
-    parser.add_argument("-v", "--voice", type=str, default=SPEAKER_WAV)
-    args = parser.parse_args()
+    """ Start processes for recording and audio output, initialize voice assist model and start main loop 
+    """
+    # !! Make sure to start multiprocessing before using any pytorch tensors to prevent GPU memory problems !! 
 
-    # Initialize models
-    whisper_model = whisper.load_model(args.whisper)
-    llm = Llama(model_path=args.llm, n_gpu_layers=30)
-    tts = TTS(args.tts, gpu=args.device=="cuda")
-    model = Voice2Voice(whisper_model, llm, tts, args.voice)
-    print("\n\n Using Cuda? ", args.device=="cuda")
-    model.run()
+    # start multiprocesses for sound input
+    audio_buffer = multiprocessing.Queue() 
+    start_recording = multiprocessing.Value('i', 0)
+    record_process = multiprocessing.Process(target=record, args=(audio_buffer,start_recording))
+    record_process.start()
 
+    # start multiprocesses for sound output
+    audio_output_buffer = multiprocessing.Queue()
+    play_audio_process = multiprocessing.Process(target=play_audio, args=(audio_output_buffer,))
+    play_audio_process.start()
 
+    # initialize buffer for processed audio input
+    streaming_buffer = StreamBuffer(chunk_size=16, shift_size=16)
+
+    # get device
+    if torch.cuda.is_available():
+        device = 'cuda'
+        # flush GPU memory
+        flush()
+    else:
+        device = 'cpu'
+        
+    # init STT-LLM-TTS pipeline
+    model = STT_LLM_TTS(device=device)
+
+    # start inference
+    main_loop(streaming_buffer, model, audio_buffer, audio_output_buffer,  start_recording)
+   
 if __name__ == "__main__":
     main()
